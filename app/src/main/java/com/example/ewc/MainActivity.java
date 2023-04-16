15618dfab4a2f1fe1829db6efcda308f297e2152
JaeMinHW <bosik99@naver.com>
2023-04-16 17:49:14 +0900

    add GPS Function file

--- app/src/main/java/com/example/ewc/MainActivity.java
+++ app/src/main/java/com/example/ewc/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private Button login;
@@ -15,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     private Button prosignup;
     private EditText id;
     private EditText pw;
+
+    String id_value;
+    String pw_value;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +36,28 @@ public class MainActivity extends AppCompatActivity {
         prosignup=  findViewById(R.id.prosignup);
         id = findViewById(R.id.userID);
         pw = findViewById(R.id.PW);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Login();
+            }
+        });
+
     }
 
     protected void Login() {
-        String id_value = id.getText().toString();
-        String pw_value = pw.getText().toString();
+        id_value = id.getText().toString();
+        pw_value = pw.getText().toString();
+
+
+
         //아이디 비밀번호 칸이 비어있지 않으면
         if(id_value.length() != 0 && pw_value.length() != 0) {
             Log.e("ID", id_value);
             Log.e("PW", pw_value);
+            RequestThread thread = new RequestThread();
+            thread.start();
         }
         //아이디 및 비밀번호 칸이 비어있으면
         else {
@@ -48,6 +72,54 @@ public class MainActivity extends AppCompatActivity {
     protected void proSignUp() {
         Intent intent = new Intent(getApplicationContext(), ProSignup.class);
         startActivity(intent);
+    }
+
+
+    class RequestThread extends Thread {
+
+//        String IP = "아이피 주소 + 포트 + / 서버에 전송하는 규칙" + "tt";
+        String IPadd = "http://54.180.101.143:5001/";
+        String IP = IPadd + "login/"+id_value+"/"+pw_value;
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(IP);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                if(conn != null){
+                    conn.setConnectTimeout(10000); // 10초 동안 기다린 후 응답이 없으면 종료
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+
+                    int resCode = conn.getResponseCode();
+
+                    if(resCode == HttpURLConnection.HTTP_OK){
+                        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while ((line = in.readLine()) != null) {
+                            response.append(line);
+                        }
+                        in.close();
+
+                        // 응답 데이터 처리
+                        String responseData = response.toString();
+                        if (responseData.equals("success")) {
+                            // 서버에서 success를 반환한 경우
+                            // 로그인 성공
+                            Log.d("MainActivity", "서버에서 success를 반환했습니다.");
+
+                        } else {
+                            // 다른 응답을 반환한 경우
+                            // 로그인 실패
+                            Toast.makeText(MainActivity.this, "아이디와 비밀번호를 확인하여주세요", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    conn.disconnect();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
